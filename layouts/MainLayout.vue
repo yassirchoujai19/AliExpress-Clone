c7385b81e2d063a576aac58f5d30a695e253b0e2
Yassir Choujai <113991646+yassirchoujai19@users.noreply.github.com>
2025-06-02 21:07:36 +0100

    Footer is added successfuly

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -88,7 +88,10 @@
         >
             <div class="flex lg:justify-start justify-between gap-10 max-w-[1150px] w-full px-3 py-5 mx-auto">
                 <NuxtLink to="/" class="min-w-[170px]">
-                   
+                     <img 
+                        width="170"
+                        src="/AliExpress-logo.png"
+                    >
                 </NuxtLink>
 
                 <div class="max-w-[700px] w-full md:block hidden">
@@ -137,9 +140,42 @@
                         </div>
                     </div>
                 </div>
-                
-                
-
+                <NuxtLink 
+                    to="/shoppingcart"
+                    class="flex items-center"
+                >
+                    <button 
+                        class="relative md:block hidden"
+                        @mouseenter="isCartHover = true"
+                        @mouseleave="isCartHover = false"
+                    >
+                        <span 
+                            class="
+                                absolute 
+                                flex 
+                                items-center 
+                                justify-center 
+                                -right-[3px] 
+                                top-0 
+                                bg-[#FF4646] 
+                                h-[17px] 
+                                min-w-[17px] 
+                                text-xs 
+                                text-white
+                                px-0.5 
+                                rounded-full
+                            "
+                        >
+                        </span>
+                        <div class="min-w-[40px]">
+                            <Icon 
+                                name="ph:shopping-cart-simple-light" 
+                                size="33" 
+                                :color="isCartHover ? '#FF4646' : ''"
+                            />
+                        </div>
+                    </button>
+                </NuxtLink>
                 <button 
                     @click="userStore.isMenuOverlay = true"
                     class="md:hidden block rounded-full p-1.5 -mt-[4px] hover:bg-gray-200"
@@ -149,16 +185,26 @@
             </div>
         </div>
     </div>
-
+        <Loading v-if="userStore.isLoading" />
 
     <div class="lg:pt-[150px] md:pt-[130px] pt-[80px]" />
     <slot />
 
-    
+        <Footer v-if="!userStore.isLoading"/>
+
 
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import { useUserStore } from '~/stores/user'
+
+const userStore = useUserStore()
+
 let isAccountMenu = ref(false)
+let isCartHover = ref(false)
+let isSearching = ref(false)
+let searchItem = ref('')
+let items = ref(null)
 
 </script>
---
+++ stores/user.js
@@ -0,0 +1,13 @@
+
+// stores/counter.js
+import { defineStore } from 'pinia'
+
+export const useUserStore = defineStore('user', {
+  state: () => ({
+    isMenuOverlay: false,
+    isLoading: false,
+    cart: [],
+    checkout: []
+  }),
+  persist: true
+})
